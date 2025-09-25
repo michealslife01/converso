a9857aae522c8cff14900d1da409ef618f323bdd
michealslife01 <michealadejimi07@gmail.com>
2025-09-25 17:38:03 -0400

    build the second

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,9 +1,16 @@
+import CompanionForm from '@/components/companionform'
 import React from 'react'
 
 const newCompanion = () => {
   return (
-    <div>newCompanion</div>
+    <main className="companion-form-container min-lg:w-1/3 min-md:w-2/4\3 items-center justify-center">
+    <article className="w-full gap-4 flex flex-col">
+        <h1>Build a New Companion</h1>
+        <p>Pick a name, subject, voice, & personality and start learning through voice conversations that feel natural and fun.</p>
+        
+        <CompanionForm />
+    </article>
+    </main>
   )
 }
-
 export default newCompanion
--- components/companionform.tsx
+++ components/companionform.tsx
@@ -1,8 +1,140 @@
-import React from 'react'
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { z } from "zod"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { subjects } from "@/constants"
+ 
+const formSchema = z.object({
+  name: z.string().min(1, {message:'companion is requred.'}),
+  subject: z.string().min(1, {message:'subject is required.'}),
+  topic: z.string().min(1, {message: 'topic is required.'}),
+  voice: z.string().min(1, {message: 'voice is required.'}),
+  style: z.string().min(1, {message: 'style is required.'}),
+  duration:z.coerce.number().min(1, {message: 'duration is required.'}),
+
+
+})
+
 
 const CompanionForm = () => {
+    // 1. Define your form.
+  const form = useForm<z.input<typeof formSchema>, any, z.output<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      subject: "",
+      topic: "",
+      voice: "",
+      style: "",
+      duration: 15,
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  const onSubmit: SubmitHandler<z.output<typeof formSchema>> = (values) => {
+    console.log(values)
+  }
+
   return (
-    <div>CompanionForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Companion name</FormLabel>
+              <FormControl>
+                <Input placeholder="enter the compnion name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Subject</FormLabel>
+              <FormControl>
+              <Select onValueChange={field.onChange} value={field.value}
+              defaultValue={field.value}>
+                <SelectTrigger className="input capitalize">
+                    <SelectValue placeholder="Select a subject" />
+                </SelectTrigger>
+                <SelectContent>
+                    {subjects.map((subject: string) => (
+                        <SelectItem value={subject} key={subject} 
+                        className="capitalize">
+                            {subject}
+                        </SelectItem>
+                    ))}
+                </SelectContent>
+              </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Companion name</FormLabel>
+              <FormControl>
+                <Input placeholder="enter the compnion name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Companion name</FormLabel>
+              <FormControl>
+                <Input placeholder="enter the compnion name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Companion name</FormLabel>
+              <FormControl>
+                <Input placeholder="enter the compnion name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+        <Button type="submit">Submit</Button>
+      </form>
+      
+    </Form>
   )
 }
 
